2ba890a83485e13634d3c0d9c0f28efd220f4fae
Virginie Khaldi <virginie.allaghenkhaldi@gmail.com>
2020-07-07 13:08:20 +0200

    formulaire contact mail admin + bascule de tout le header + body base.html.twig vers home / base contenant only navbar stylesheet + footer

---
+++ src/Notification/ContactNotification.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Notification;
+
+use App\Entity\Contact;
+use Twig\Environment;
+
+class ContactNotification
+{
+    /**
+    * @var \Swift_Mailer
+    */
+    private $mailer;
+
+    /**
+    * @var Environment
+    */
+    private $renderer;
+
+    public function __construct(\Swift_Mailer $mailer, Environment $renderer)
+    {
+        $this->mailer = $mailer;
+        $this->renderer = $renderer;
+    }
+
+    public function notify(Contact $contact)
+    {
+        $message = (new \Swift_Message('Message : ' . $contact->getMessage()))
+                ->setFrom($contact->getEmail())
+                ->setTo('virginie.allaghenkhaldi@gmail.com')
+                ->setReplyTo($contact->getEmail())
+                ->setBody($this->renderer->render('emails/contact.html.twig', [
+                'contact' => $contact
+                ]), 'text/html');
+                
+        $this->mailer->send($message);
+    }
+
+}
